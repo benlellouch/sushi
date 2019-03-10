e67f1865a5df12d89552bdda501e36a56f10624a
benlellouch <blellouch@gmail.com>
2019-03-10 14:43:46 +0000

    I am going to rewrite the recipe tab as it doesn't need all that info. I am also going to implement a listener on the dish table so that the recipe table updates every time I select a specific dish.

--- src/main/java/comp1206/sushi/Tabs/AddDishTab.java
+++ src/main/java/comp1206/sushi/Tabs/AddDishTab.java
@@ -1,34 +1,76 @@
 package comp1206.sushi.Tabs;
 
+import comp1206.sushi.common.Dish;
 import comp1206.sushi.server.ServerInterface;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.scene.control.Button;
+import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
-import javafx.scene.layout.GridPane;
+import javafx.scene.layout.VBox;
+
+import java.text.NumberFormat;
+import java.text.ParseException;
 
 public class AddDishTab extends MainTab {
     ServerInterface server;
+    private ObservableList<Dish> dishObservableList;
+    private TableView<Dish> dishTableView;
     private TextField nameInput;
     private TextField descriptionInput;
     private TextField priceInput;
     private TextField restockTInput;
     private TextField restockAInput;
 
-    public AddDishTab (String name, ServerInterface server){
+
+    public AddDishTab (String name, ServerInterface server, ObservableList<Dish> dishObservableList, TableView<Dish> dishTableView){
         super(name);
         this.server = server;
-        GridPane superPane = new GridPane();
+        this.dishObservableList = dishObservableList;
+        this.dishTableView = dishTableView;
+        VBox superPane = new VBox();
         nameInput = new TextField();
         descriptionInput = new TextField();
         priceInput = new TextField();
         restockTInput = new TextField();
         restockAInput = new TextField();
+
         Button addButton = new Button("Add Dish");
+        addButton.setOnAction(event -> addButtonClicked());
+
         Button removeButton = new Button("Remove Dish");
+        removeButton.setOnAction(event -> removeButtonClicked());
+
         superPane.getChildren().addAll(nameInput,descriptionInput,priceInput,restockAInput,restockTInput,addButton,removeButton);
-        superPane.addRow(0, nameInput, descriptionInput, priceInput);
-        superPane.addRow(1, restockAInput, restockTInput);
-        superPane.addRow(2, addButton, removeButton);
+//        superPane.addRow(0, nameInput, descriptionInput, priceInput);
+//        superPane.addRow(1, restockAInput, restockTInput);
+//        superPane.addRow(2, addButton, removeButton);
         this.setContent(superPane);
 
+    }
+
+    public void addButtonClicked(){
+        try {
+            server.addDish(nameInput.getText(), descriptionInput.getText(), NumberFormat.getInstance().parse(priceInput.getText()), NumberFormat.getInstance().parse(restockTInput.getText()), NumberFormat.getInstance().parse(restockAInput.getText()));
+            dishObservableList = FXCollections.observableArrayList(server.getDishes());
+            dishTableView.setItems(dishObservableList);
+            nameInput.clear();
+            descriptionInput.clear();
+            priceInput.clear();
+            restockAInput.clear();
+            restockTInput.clear();
+        } catch (ParseException e){
+            System.out.println("Was unable to parse the numbers entered. ");
+        }
+    }
+
+    public void removeButtonClicked(){
+        try{
+            server.removeDish(dishTableView.getSelectionModel().getSelectedItem());
+            dishObservableList = FXCollections.observableArrayList(server.getDishes());
+            dishTableView.setItems(dishObservableList);
+        }catch (ServerInterface.UnableToDeleteException e){
+            System.out.println("was unable to remove that");
+        }
     }
 }
--- src/main/java/comp1206/sushi/Tabs/DishTab.java
+++ src/main/java/comp1206/sushi/Tabs/DishTab.java
@@ -4,6 +4,8 @@ import comp1206.sushi.common.Dish;
 import comp1206.sushi.common.Ingredient;
 import comp1206.sushi.common.Supplier;
 import comp1206.sushi.server.ServerInterface;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.scene.control.*;
@@ -31,7 +33,7 @@ public class DishTab extends MainTab {
         this.server = server;
 
         dishObservableList = FXCollections.observableArrayList(server.getDishes());
-        ingredientObservableList = FXCollections.observableList(server.getIngredients());
+//        ingredientObservableList = FXCollections.observableList(server.getIngredients());
 
         HBox superBox = new HBox();
 
@@ -57,6 +59,13 @@ public class DishTab extends MainTab {
 
         dishTableView.getColumns().addAll(dishNameColumn, descriptionColumn,priceColumn,dishRestockThresholdColumn,dishRestockAmountColumn);
         dishTableView.setItems(dishObservableList);
+        dishTableView.getSelectionModel().selectedItemProperty().addListener(new ChangeListener<Dish>() {
+            @Override
+            public void changed(ObservableValue<? extends Dish> observableValue, Dish dish, Dish t1) {
+                ingredientTableView.setItems(FXCollections.observableArrayList(server.getIngredients()));
+            }
+        });
+
 
         // Table columns for Ingredient Table
         TableColumn<Ingredient,String> nameColumn = new TableColumn<>("Name");
@@ -75,12 +84,13 @@ public class DishTab extends MainTab {
         restockAmountColumn.setCellValueFactory(new PropertyValueFactory<>("restockAmount"));
 
         ingredientTableView.getColumns().addAll(nameColumn, unitColumn, supplierColumn,restockThresholdColumn,restockAmountColumn);
-        ingredientTableView.setItems(ingredientObservableList);
+//        ingredientTableView.setItems(ingredientObservableList);
 
         // Setting up the Add new Dish tab and the edit recipe tab
         TabPane addAndEditTab = new TabPane();
-        MainTab addTab = new AddDishTab("Add Dish", server);
+        MainTab addTab = new AddDishTab("Add Dish", server, dishObservableList, dishTableView);
 //        MainTab addTab = new MainTab("Add Placeholder");
+//        MainTab addTab = createAddDishTab();
         Tab editTab = new Tab("Edit Placeholder");
         addAndEditTab.getTabs().addAll(addTab,editTab );
 
@@ -88,5 +98,34 @@ public class DishTab extends MainTab {
         superBox.getChildren().addAll(dishTableView, recipeAndInputBox);
         this.setContent(superBox);
 
+
     }
+
+    public ObservableList<Dish> getDishObservableList() {
+        return dishObservableList;
+    }
+
+    public void setDishObservableList(ObservableList<Dish> dishObservableList) {
+        this.dishObservableList = dishObservableList;
+    }
+
+    //    public MainTab createAddDishTab(){
+//        MainTab addDishTab = new MainTab("Add Dish");
+//        GridPane superPane = new GridPane();
+//        nameInput = new TextField();
+//        descriptionInput = new TextField();
+//        priceInput = new TextField();
+//        restockTInput = new TextField();
+//        restockAInput = new TextField();
+//        Button addButton = new Button("Add Dish");
+//        Button removeButton = new Button("Remove Dish");
+//        superPane.getChildren().addAll(nameInput,descriptionInput,priceInput,restockAInput,restockTInput,addButton,removeButton);
+////        superPane.addRow(0, nameInput, descriptionInput, priceInput);
+////        superPane.addRow(1, restockAInput, restockTInput);
+////        superPane.addRow(2, addButton, removeButton);
+//        addDishTab.setContent(superPane);
+//        return addDishTab;
+//    }
+
+
 }
